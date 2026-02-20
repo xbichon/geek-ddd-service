3cc5632cb070c9b29c7864cfe801b52bd6a72f73
xbichon@163.com <xbichon@163.com>
2026-02-20 16:18:54 +0800

    refactor(config): 启用应用延迟初始化配置
    - 在 application-dev.yml 中启用 spring.main.lazy-initialization 配置
    - 为 ApplicationUtil 组件添加 @Lazy(false) 注解确保非懒加载
    - 导入 org.springframework.context.annotation.Lazy 依赖包

--- infrastructure/src/main/java/vip/geekclub/framework/utils/ApplicationUtil.java
+++ infrastructure/src/main/java/vip/geekclub/framework/utils/ApplicationUtil.java
@@ -3,6 +3,7 @@ package vip.geekclub.framework.utils;
 import lombok.NonNull;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ import java.util.Map;
  * @author xiongrui
  */
 @Component
+@Lazy(false)
 public class ApplicationUtil implements ApplicationContextAware {
 
     private static ApplicationContext applicationContext;
